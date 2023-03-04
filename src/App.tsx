8edd6a3eedc4d56a98707bfc45c3df323103ed36
DrigaNicolai <just.whyme7@gmail.com>
2023-03-04 16:09:26 +0200

    Install tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
-import React from 'react';
+import React, {createElement as e} from 'react';
 
 function App() {
-  return (
+  /*return (
     <h1>Hello React</h1>
-  );
+  );*/
+  return e("div", {className: "container"}, [
+    e("h1", {className: "font-bold"}, "Test JSX"),
+    e("button", {className: ""}, "Click me!")
+  ])
 }
 
 export default App;
