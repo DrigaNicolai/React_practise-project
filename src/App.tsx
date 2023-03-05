438a8d8ae3c66924f0e32322d12c9739b8354475
DrigaNicolai <just.whyme7@gmail.com>
2023-03-05 13:46:14 +0200

    Add open/close functionality to Modal window

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ import {IProduct} from "./models";
 
 function App() {
   const {loading, products, error, addProduct} = useProducts();
-  const [modal, setModal] = useState(true);
+  const [modal, setModal] = useState(false);
 
   const createHandler = (product: IProduct) => {
     setModal(false);
@@ -29,10 +29,20 @@ function App() {
       ) }
 
       { modal &&
-        <Modal title={"Create new product"}>
+        <Modal
+          title={"Create new product"}
+          onClose={() => setModal(false)}
+        >
           <CreateProduct onCreate={createHandler} />
         </Modal>
       }
+
+      <button
+        className="fixed bottom-5 right-5 rounded-full bg-red-700 text-white text-2xl px-4 py-2"
+        onClick={() => setModal(true)}
+      >
+        Add product
+      </button>
     </div>
   )
 }
